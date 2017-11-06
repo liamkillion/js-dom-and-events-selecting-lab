0377cae66f85bc22cfe5ec9b0294a10614913dc5
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2017-11-06 09:59:36 -0500

    update to include getbyId and getbyClassName

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ function rankedLis() {
   return document.querySelectorAll('.ranked-list li')
 }
 
+function selectElementById(id){
+  return document.getElementById(id)
+}
+
+function selectElementsByClassName(className){
+  return document.getElementsByClassName(className)
+}
+
 function companyStrategy() {
   let node = document.getElementById('grand-node')
   let nextNode = node.children[0]
